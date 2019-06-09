c64b2f993813918e6cb0559d7c31879e3742b232
Anton <a.alexeyev@mail.ru>
2019-06-09 20:01:48 +0300

    Первая версия сервера

--- main.js
+++ main.js
@@ -1,83 +1,37 @@
-const { promisify } = require('util');
-const fs = require('fs');
-const filenames = process.argv.slice(2);
-const walker = require('./libs/walker');
-const path = require('path');
-const mkdirSync = promisify(fs.mkdirSync);
-const existsSync = promisify(fs.existsSync);
-const linkSync = promisify(fs.linkSync);
-const unlinkSync = promisify(fs.unlinkSync);
-const rmdirSync = promisify(fs.rmdirSync);
-
-if (filenames.length < 2) {
-  console.error('Должно быть задано не менее 2-х параметров');
-  process.exit(1);
-}
-
-const src = filenames[0];
-const dist = filenames[1];
-const del =
-  filenames.length === 2 ? true : filenames[2].toLowerCase().trim() === 'true';
-
-try {
-  !(fs.existsSync(dist)) && fs.mkdirSync(dist);
-} catch (err) {
-  console.error(err.message);
-  process.exit(1);
-}
-
-const copyFiles = (item, localBase, done) => {
-  try {
-    // создаем папку-приемник по первой букве файла
-    let firstWord = item.charAt(0).toUpperCase();
-    let dir = path.join(dist, firstWord);
-    if (!(fs.existsSync(dir))) {
-      fs.mkdirSync(dir);
-    }
-    console.log('Создали папку ', dir);
-    // копируем файл в папку-приемник
-    let newFile = path.join(dir, item);
-    fs.linkSync(localBase, newFile);
-    console.log('Скопировали файл ', newFile);
-    done();
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-const moveFiles = (item, localBase, done) => {
-  try {
-    copyFiles(item, localBase, done);
-    // удаляем файл источник
-    console.log('Пытаемся удалить файл ', localBase);
-    del && fs.unlinkSync(localBase);
-    console.log('Удалили файл ', localBase);
-    done();
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-const delFolder = (localBase, done) => {
-  try {
-    // удаляем папку источник
-    fs.rmdirSync(localBase);
-    console.log('Удалили папку ', localBase);
-    done();
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-walker(src, del ? delFolder : null, del ? moveFiles : copyFiles)
-  .then(() => {
-    del && fs.rmdirSync(src);
+const yargs = require('yargs');
+const http = require('http');
+const port = 3000;
+const argv = yargs
+  .usage('Usage: $0 [options]')
+  .help('help')
+  .alias('help', 'h')
+  .version('0.0.1')
+  .alias('version', 'v')
+  .example('$0 --interval 2000 --duration 6000', '--> через interval (мсек) происходит вывод в консоль времени, прекращение вывода через duration (мсек)')
+  .option('interval', {
+    alias: 'i',
+    describe: 'Интревал в мсек, через который будет происходить вывод в консоль времени',
+    demandOption: true
   })
-  .catch(err => {
-    console.error(err.message);
-    process.exit(1);
+  .option('duration', {
+    alias: 'd',
+    describe: 'Длительность в мсек, через которую выводит в консоль прекратится',
+    demandOption: true
   })
-  .finally(() => console.log('Done'));
+  .epilog('Node.js 1st week meeting')
+  .argv;
+
+const timerId = setInterval(() => {
+  console.log(Date());
+}, parseInt(argv.interval));
+
+const server = http.createServer((req, res) => {
+  setTimeout(() => {
+    clearInterval(timerId);
+    res.end(Date());
+  }, parseInt(argv.duration));
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
